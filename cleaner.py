937115ba2a682adf1da9c958757ff2351f03a3d0
m0rn <morn88@gmail.com>
2016-04-21 20:18:26 +0300

    Простой скрипт для сортировки загрузок в Windows

--- cleaner.py
+++ cleaner.py
@@ -6,9 +6,11 @@ from logging import shutdown
 
 
 def clean():
+    home = os.path.expanduser('~')
+    print(home)
     print("Сбор данных...")
     path_j = os.getcwd()
-    list_of_files = os.listdir(r'C:\Users\ЭМПРиО\Downloads')
+    list_of_files = os.listdir(os.path.join(home, r'Downloads'))
     list_of_images = []
     list_of_video = []
     list_of_music = []
@@ -16,13 +18,14 @@ def clean():
 
 
     for i in list_of_files:
-        if i.endswith('.jpg') | i.endswith('.jpeg') | i.endswith('png'):
+        if i.lower().endswith('.jpg') | i.lower().endswith('.jpeg') |\
+                i.lower().endswith('.png') | i.lower().endswith('.gif'):
             list_of_images.append(i)
-        elif i.endswith('.webm'):
+        elif i.lower().endswith('.webm') | i.lower().endswith('.mp4'):
             list_of_video.append(i)
-        elif i.endswith('.mp3'):
+        elif i.lower().endswith('.mp3'):
             list_of_music.append(i)
-        elif i.endswith('.pdf'):
+        elif i.lower().endswith('.pdf'):
             list_of_docs.append(i)
 
     print('Количество файлов "Images: %s"\n'
@@ -32,8 +35,16 @@ def clean():
           (len(list_of_images), len(list_of_video), len(list_of_music), len(list_of_docs)))
 
     for i in list_of_images:
-        shutil.move(os.path.join(r'C:\Users\ЭМПРиО\Downloads', i), r'C:\Users\ЭМПРиО\Pictures')
+        shutil.move(os.path.join(os.path.join(home, 'Downloads'), i), os.path.join(home, 'Pictures'))
 
+    for i in list_of_music:
+        shutil.move(os.path.join(os.path.join(home, 'Downloads'), i), os.path.join(home, 'Music'))
+
+    for i in list_of_docs:
+        shutil.move(os.path.join(os.path.join(home, 'Downloads'), i), os.path.join(home, 'Documents'))
+
+    for i in list_of_video:
+        shutil.move(os.path.join(os.path.join(home, 'Downloads'), i), os.path.join(home, 'Videos'))
 if __name__ == '__main__':
     print("Начало очистки папки...")
     clean()
