27a701692385322770ea0d63a255dddc712bc68a
m0rn <morn88@gmail.com>
2016-04-22 08:37:00 +0300

    Внесены изменения: перенос torrent файлов

--- cleaner.py
+++ cleaner.py
@@ -4,15 +4,16 @@ import os
 
 def clean():
     home = os.path.expanduser('~')
+    path_to_distr = r'F:\Distrib'
     print(home)
     print("Сбор данных...")
-    path_j = os.getcwd()
     list_of_files = os.listdir(os.path.join(home, r'Downloads'))
     list_of_images = []
     list_of_video = []
     list_of_music = []
     list_of_docs = []
     list_of_torr = []
+    list_of_exe = []
 
     for i in list_of_files:
         if i.lower().endswith('.jpg') | i.lower().endswith('.jpeg') |\
@@ -24,6 +25,8 @@ def clean():
             list_of_music.append(i)
         elif i.lower().endswith('.pdf'):
             list_of_docs.append(i)
+        elif i.lower().endswith('.exe'):
+            list_of_exe.append(i)
         elif i.lower().endswith('.torrent'):
             list_of_torr.append(i)
 
@@ -31,8 +34,14 @@ def clean():
           'Количество файлов "Video: %s"\n'
           'Количество файлов "Music: %s"\n'
           'Количество файлов "Torrents: %s"\n'
+          'Количество файлов "Exe: %s"\n'
           'Количество файлов "Docs: %s"\n' %
-          (len(list_of_images), len(list_of_video), len(list_of_music), len(list_of_torr), len(list_of_docs)))
+          (len(list_of_images),
+           len(list_of_video),
+           len(list_of_music),
+           len(list_of_torr),
+           len(list_of_exe),
+           len(list_of_docs)))
 
     for i in list_of_images:
         shutil.move(os.path.join(os.path.join(home, 'Downloads'), i), os.path.join(home, 'Pictures'))
@@ -49,6 +58,13 @@ def clean():
     for i in list_of_torr:
         shutil.move(os.path.join(os.path.join(home), 'Downloads'), i),\
         os.path.join(home, os.path.join('Downloads', 'torr'))
+    for i in list_of_exe:
+        try:
+            shutil.move(os.path.join(os.path.join(home, 'Downloads'), i), path_to_distr)
+        except:
+            pass
+
+
 if __name__ == '__main__':
     print("Начало очистки папки...")
     clean()
