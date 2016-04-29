cb5e62bc37ef09dac7627fedaf79c1cd23b9f618
m0rn <morn88@gmail.com>
2016-04-29 16:30:33 +0300

    Дополнен файл статистики

--- my_stat.py
+++ my_stat.py
@@ -1,7 +1,17 @@
 import statistics
 
-example_list = [4, 6, 2, 6, 7, 8, 2, 5, 6, 78, 4, 6, 7, 2, 2]
+example_list = [4, 6, 2, 6, 7, 8, 2, 5, 6, 4, 6, 7, 2, 2]
+example_list2 = [1, 2, 3, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+example_list3 = [1, 100, 101, 2]
 x = statistics.mean(example_list)
 y = statistics.median(example_list)
 z = statistics.stdev(example_list)
-print(x, y, z)
+a = statistics.median_low(example_list)
+b = statistics.median_high(example_list)
+c = statistics.variance(example_list)
+print(x, y, z, a, b)
+print(c)
+d = statistics.variance(example_list2)
+e = statistics.variance(example_list3)
+print(d)
+print(e)
